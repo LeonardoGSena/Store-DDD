2e9c8b06b0aa0d91dd61013129e0557ec919f5e0
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-09-21 16:58:36 -0300

    Store - DDD Studies

---
+++ Store.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+using Store.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Store.Domain.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Get(IEnumerable<Guid> ids);
+    }
+}
